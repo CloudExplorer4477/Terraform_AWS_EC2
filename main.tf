6f4b382b0bb5c7fc6ffa39365ade31d132cfcb8e
Pavan Kumar K J <Pavankumarkj347@Gmail.com>
2026-05-30 16:42:30 +0530

    Terraform Code

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+
+  profile = "terraform"
+}
+
+resource "aws_instance" "kubernetes_server" {
+  ami               = "ami-0038df39db13a87e2"
+  instance_type     = "t2.medium"
+  availability_zone = "ap-south-1a"
+  key_name          = "Cloud"
+
+  tags = {
+    Name = "Kubernetes_Server"
+  }
+}
